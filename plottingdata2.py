8c7dfb99f9bd509f8ea3f5e000bfe270ee574d9b
emmalamon <156194112+emmalamon@users.noreply.github.com>
2025-03-19 18:53:10 +0000

    Add files via upload

--- plottingdata2.py
+++ plottingdata2.py
@@ -14,7 +14,11 @@ plt.bar(X, Y, color='b')
 plt.title("Sleep hours in relation to attendance ") 
 plt.xlabel("Sleep hours") 
 plt.ylabel("Attendance(%)") 
-  
+
+#Save the bar chart as a png file
+plt.savefig('bar_chart.png')
+
+
 # Show the plot 
 plt.show()
 
@@ -25,6 +29,21 @@ Y = list(df.iloc[:,4])
 plt.scatter(X, Y, color='g') 
 plt.title("Study hours in relation to grades") 
 plt.xlabel("Study hours") 
-plt.ylabel("Grades") 
+plt.ylabel("Grades")
+
+plt.savefig('scatter_chart.png')
 plt.show()
+
+
+X = list(df.iloc[:,1])
+Y = list(df.iloc[:,3])
+
+plt.scatter(X, Y, color='r') 
+plt.title("Sleep in relation to attendance") 
+plt.xlabel("Sleep") 
+plt.ylabel("Attendance")
+
+plt.savefig('scatter_chart2.png')
+plt.show()
+
 
